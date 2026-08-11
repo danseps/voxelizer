1c3189d2f09f10a9a86e6a7881cf0a533e70a62e
danseps <dan.sepa@gmail.com>
2026-08-11 13:54:18 +0200

    Diffuse lighting

--- res/shaders/fragmentShader.fs
+++ res/shaders/fragmentShader.fs
@@ -3,11 +3,24 @@
 
 out vec4 FragColor;
 
+in vec3 fragPos;
 in vec4 vertexColor;
+in vec3 normal;
 
 uniform vec3 lightColor;
+uniform vec3 lightPos;
 
 void main()
 {
-    FragColor = vertexColor * vec4(lightColor, 1.0);
+    // ambient
+    float ambientStrength = 0.15;
+    vec3 ambient = ambientStrength * lightColor;
+
+    // diffuse
+    vec3 norm = normalize(normal);
+    vec3 lightDir = normalize(lightPos - fragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    FragColor = vertexColor * vec4(ambient + diffuse, 1.0);
 }
--- res/shaders/vertexShader.vs
+++ res/shaders/vertexShader.vs
@@ -3,8 +3,12 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aCol;
+layout (location = 2) in vec3 aNormal;
+layout (location = 3) in vec2 uv; //TODO:
 
+out vec3 fragPos;
 out vec4 vertexColor;
+out vec3 normal;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -14,4 +18,6 @@ void main()
 {
     gl_Position = proj * view * model * vec4(aPos, 1.0);
     vertexColor = aCol;
+    fragPos = vec3(model * vec4(aPos, 1.0));
+    normal = aNormal;
 }
